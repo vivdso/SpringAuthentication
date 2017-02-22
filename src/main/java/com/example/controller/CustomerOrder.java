c23b0e5a266f39ee68857f03008f6478cb96830e
Vivek D'Souza <vivek.d'souza@equifax.com>
2017-02-21 22:18:14 -0600

    no change

--- src/main/java/com/example/controller/CustomerOrder.java
+++ src/main/java/com/example/controller/CustomerOrder.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/order")
+
 public class CustomerOrder {
 
     @RequestMapping(method = RequestMethod.GET)
